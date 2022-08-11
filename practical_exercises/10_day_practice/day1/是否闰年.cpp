0b146204d3226d03d9de708397a59fe723c1624c
lingxiao C <2423450548@qq.com>
2022-08-11 09:01:22 +0000

    十日狂练部分的学习

--- practical_exercises/10_day_practice/day1/是否闰年.cpp
+++ practical_exercises/10_day_practice/day1/是否闰年.cpp
@@ -6,14 +6,17 @@ int main(int argc, char const *argv[])
     bool isLeapYear;
     cout<<"Enter the year: ";
     cin>>year;
-    isLeapYear = (((year%4==0)&&(year%100!=0))||(year%400==0));
+
+/*   1、普通闰年：公历年份是4的倍数的，一般是闰年。（如2004年就是闰年）；
+     2、世纪闰年：公历年份是整百数的，必须是400的倍数才是闰年（如1900年不是世纪闰年，2000年是世纪闰年）。*/
+    isLeapYear = (((year%4==0)&&(year%100!=0))||(year%400==0));  
     if(isLeapYear)
     {
-        cout<<year<<" is a leap year"<<endl;
+        cout<<year<<" is a leap year"<<endl;   //润年 
     }
     else
     {
-        cout<<year<<" is not a leap year"<<endl;
+        cout<<year<<" is not a leap year"<<endl;  //不润年
     }
     system("pause");
     return 0;
--- practical_exercises/10_day_practice/day1/联合体学习.cpp
+++ practical_exercises/10_day_practice/day1/联合体学习.cpp
@@ -5,6 +5,7 @@ union myun   //只分配一份其中最大成员的空间 u
 {
     struct { int x; int y; int z; }u; 
     int k; 
+    int h;
 }a; 
 
 int main() 
@@ -13,8 +14,10 @@ int main()
     a.u.y =5; 
     a.u.z =6; 
     a.k = 0; //空间只有u大小  覆盖掉第一个int空间值 
+    a.h = 1;
     
-    printf("%d %d %d %d\n",a.u.x,a.u.y,a.u.z,a.k);
+    printf("%d %d %d %d %d\n",a.u.x,a.u.y,a.u.z,a.k,a.h);
+    printf("地址: %d %d %d %d %d %d %d \n", &a, &a.u , &a.u.x, &a.u.y, &a.u.z, &a.k, &a.h);
     system("pause");
     return 0;
 }
